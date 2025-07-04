d9db198eeb4e8f1fdaf200d455e2e5ba72a7aeed
Moritz Eysholdt <moritz@gitpod.io>
2025-07-04 12:03:21 +0000

    added troubleshooting script

---
+++ compose/troubleshoot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+MISSING_FILE="init-db.sh"
+
+echo "=== Finding $MISSING_FILE in the host ==="
+docker run --rm -v /:/host alpine sh -c "find / -type f -name '$MISSING_FILE' 2>/dev/null"
+echo ""
+
+echo "=== $MISSING_FILE in the container ==="
+docker compose exec postgres-15 sh -c "find / -type f -name '$MISSING_FILE' 2>/dev/null"
+echo ""
+
+echo "===  Docker context inspect ==="
+docker context inspect
+echo ""
+
+echo "===  Docker Info ==="
+docker info
+echo ""
+
+for cid in $(docker ps -q); do
+    cname=$(docker inspect --format '{{.Name}}' "$cid" | sed 's|^/||')  # strip leading /
+    echo "=== Container $cname ==="
+    docker container inspect "$cid"
+    echo ""
+done
